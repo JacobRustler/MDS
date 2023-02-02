defec20771279aa1d7d22b261872e12cbec45918
JacobRustler <david.portilla@correounivalle.edu.co>
2023-02-02 11:35:06 -0500

    config postgres y bodyparser

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-//Crear aplicación de express
-const express = require('express');
-const res = require('express/lib/response');
-//Ejecutar aplicación de express
+require('dotenv').config()
+
+const { request } = require('express');
+const express = require('express')
+
+const bodyParser = require("body-parser");
+
+const path = require('path');
+
+const router = express.Router();
 const app = express()
-//Definicion del puerto, donde se manipulan las variables de ambiente
+
 const port = process.env.PORT || 3000;
 
-//Get
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.POSTGRESURI,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  client.connect();
+
 app.get('/master',(req,res)=>{
     res.send('Inicial')
 })
-//hehe
-//Puerto Local
+
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
 })
